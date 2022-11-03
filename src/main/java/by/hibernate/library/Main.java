4d978d20e162c17b8c174b5018e89b9eeec3d2bb
mrchernova <cherraniram@gmail.com>
2022-11-03 17:17:03 +0300

    commit base project

---
+++ src/main/java/by/hibernate/library/Main.java
@@ -0,0 +1,11 @@
+package by.hibernate.library;
+
+import java.sql.*;
+
+public class Main {
+    public static void main(String[] args) throws SQLException {
+
+        Menu.launchMenu();
+
+    }
+}

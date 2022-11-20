35834d65722dcf278eb1026654f214466abdacd5
cherraniram@gmail.com <cherraniram@gmail.com>
2022-11-20 17:14:50 +0300

    jdbc

--- src/main/java/info/spring/config/AppConfig.java
+++ src/main/java/info/spring/config/AppConfig.java
@@ -1,6 +1,5 @@
 package info.spring.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -30,11 +29,6 @@ public class AppConfig {
         return dataSource;
     }
 
-//    @Bean
-//    public JdbcTemplate jdbcTemplate(@Autowired DataSource dataSource) {   // @Autowired на всякий
-//        return new JdbcTemplate(dataSource);
-//    }
-//    либо
     @Bean
     public JdbcTemplate jdbcTemplate() {
         return new JdbcTemplate(dataSource());
--- src/main/java/info/spring/model/Author.java
+++ src/main/java/info/spring/model/Author.java
@@ -7,8 +7,9 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class Author {
 
+
+public class Author {
     private Long id;
     private String name;
 }
